a18656d534de4154a54c964b2abe062327ece346
Julia Teixeira Barbosa <juliateixeira22@hotmail.com>
2023-04-08 01:48:21 -0300

    atualização pagina como ajudar

--- script.js
+++
@@ -1,9 +0,0 @@
-function Enviar(){
-    var name = document.querySelector("#name").value
-    var email = document.querySelector("#email").value
-    if(name.length==0||email.length==0){
-        alert('[ERRO] ] Verifique os campos')
-    }else{
-        alert("Enviado, aguarde o retorno.")
-    }
-}
